3e40a287ce1713efa6080f945ee2d3249979d931
Victor 7004 <turist70@i.ua>
2020-12-17 17:15:27 +0200

    finished test_product

--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,14 @@
 from pages.product_page import ByPage
-from pages.login_page import LoginPage
-import time 
-import math
+import time
+
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ByPage(browser, link)
     page.open()
     page.go_to_button_page()
     page.solve_quiz_and_get_code()
-    time.sleep(3) 
-    # выполнить проверку добавилась ли книга 
-    # выполнить проверку правильности цены
-    page.should_be_by_busket() 
+    time.sleep(10)
+    page.compare_name()
+    page.compare_price()
