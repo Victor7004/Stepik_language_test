685da7ec0cff6393379f03a36ab48e0d15b83ed0
Victor 7004 <turist70@i.ua>
2020-12-25 19:24:54 +0200

    add user

--- test_product_page.py
+++ test_product_page.py
@@ -29,3 +29,27 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
+class TestUserAddToBasketFromProductPage(BasePage):
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self,browser):
+        login_link= "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, login_link)
+        page.open()
+        f = faker.Faker()
+        email = f.email()
+        password = str(time.time())
+        page.register_new_user(email=email,password=password)
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer1"
+        page = ByPage(browser, link)
+        page.open()
+        page.user_can_add_product_to_basket()
+
+    def test_user_cant_see_success_message(browser)
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer1"
+        page = ByPage(browser, link)
+        page.open()
+        page.user_cant_see_success_message() 
+
