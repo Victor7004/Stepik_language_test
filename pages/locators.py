7aed0d6b8ba7747fcdeffbe7359535bc95365bb9
Victor 7004 <turist70@i.ua>
2020-12-28 19:12:04 +0200

    add locator

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,4 @@ class ByPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user") 
