60b101d2cde41851f936d617db8a31ee04b59259
Victor 7004 <turist70@i.ua>
2020-12-29 19:55:42 +0200

    add _init_

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,14 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
-class MainPage(BasePage): 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click() 
-    def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+
+class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs) 
+
+#class MainPage(BasePage): 
+    #def go_to_login_page(self):
+        #login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        #login_link.click() 
+    #def should_be_login_link(self):
+        #self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
