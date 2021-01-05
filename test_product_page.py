aa2bf6b153f9e039945574cc2ad28deafdc0564b
Victor 7004 <turist70@i.ua>
2021-01-05 16:35:59 +0200

    add def

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,14 @@
 from pages.product_page import ByPage
+from pages.login_page import LoginPage
+from pages.locators import ByPageLocators
+from pages.basket_page import BasketPage
+from pages.locators import BasketPageLocators
+from pages.base_page import BasePage
+import faker
 import pytest
 import time
 import math
+
 
 product_link_by = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 links = [f"{product_link_by}/?promo=offer{number}" for number in range(10)]
@@ -17,6 +24,12 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.compare_name()
     page.compare_price()
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ByPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -25,6 +38,14 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     page.should_be_in_element_present()
     page.is_not_basket_empty()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ByPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
