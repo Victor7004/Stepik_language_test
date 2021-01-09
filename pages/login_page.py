74da5ec2cd317aaf19f24fe7832fc7bd5d2e011f
Victor 7004 <turist70@i.ua>
2021-01-09 16:13:17 +0200

    add def

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
+from selenium import webdriver
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,23 +10,20 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
+        assert 'login' in self.browser.current_url, "Cant find word login in url"
         assert True
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented "
         assert True
 
-    def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
-
-    
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*MainPageLocators. REGISTER_FORM), "Register form is not presented "
         assert True
     def register_new_user(self, email, password):
-        # реализуйте метод регистрации нового пользователя
-        wait = Webdriver(self.driver, 5)
-        wait.until(EC.presence_of_element_located(*LoginPageLocators.REGISTER_FORM))
-        self.driver.find_element((*LoginPageLocators.LOGIN_FORM).send_keys(email))
+        email_field = self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
+        email_field.send_keys(email)
+        password_field = self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
+        password_field.send_keys(password)
