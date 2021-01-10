aa04c92ca087b42cb098d01bc3d31a8d1ec87dc9
Victor 7004 <turist70@i.ua>
2021-01-10 15:46:46 +0200

    add def

--- test_product_page.py
+++ test_product_page.py
@@ -42,13 +42,6 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.is_not_basket_empty()
 
 
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ByPage(browser, link)
-    page.open()
-    page.go_to_login_page() 
-
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -56,6 +49,8 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ByPage(browser, link)
